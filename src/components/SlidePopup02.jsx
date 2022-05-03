80bbe5503ee4953ff0f99b142a6f9b2910d49d98
lee kyoungmin <kyoungminlee32@gmail.com>
2022-05-03 10:34:48 +0900

    form태그 삭제

--- src/components/SlidePopup02.jsx
+++ src/components/SlidePopup02.jsx
@@ -12,35 +12,33 @@ function SlideCenter(props) {
 					<button type="button" className="btn closeBtn" id="defaultClose" onClick={hideCenterOPen}>닫기</button>
 				</div>
 				<div data-role="content" className="modal-contents">
-					<form>
-						<section className="with-fixedBtn">
-							<label htmlFor="mailProvider">이메일</label>
-							<dl className="columnSet emailInput">
-								<dt className="colItem">
-									<input type="text" id="inputLabel4" placeholder="abcd" className="formInput" name="emailId" />
-								</dt>
-								<dd className="colItem">
-									<label htmlFor="mailProvider" className="visualHide">Email Provider</label>
-									<div className="select-input">
-										<select id="mailProvider" className="formInput" name="">
-											<option value="gmail.com" label="gmail.com">gmail.com</option>
-											<option value="hanmail.net" label="hanmail.net">hanmail.net</option>
-											<option value="naver.com" label="naver.com">naver.com</option>
-											<option value="nate.com" label="nate.com">nate.com</option>
-											<option value="직접입력" label="direct input">직접입력</option>
-										</select>
-									</div>
-								</dd>
-							</dl>
-							<div className="textInput">
-								<input type="text" id="inputLabel1" placeholder="" className="formInput" name="" />
-							</div>
-						</section>
-						<ul className="columnSet selfBottom">
-							<li className="colItem"><button type="button" className="btn primary cancel">취소</button></li>
-							<li className="colItem"><button type="submit" className="btn primary submit" onClick={hideCenterOPen}>확인</button></li>
-						</ul>
-					</form>
+					<section className="with-fixedBtn">
+						<label htmlFor="mailProvider">이메일</label>
+						<dl className="columnSet emailInput">
+							<dt className="colItem">
+								<input type="text" id="inputLabel4" placeholder="abcd" className="formInput" name="emailId" />
+							</dt>
+							<dd className="colItem">
+								<label htmlFor="mailProvider" className="visualHide">Email Provider</label>
+								<div className="select-input">
+									<select id="mailProvider" className="formInput" name="">
+										<option value="gmail.com" label="gmail.com">gmail.com</option>
+										<option value="hanmail.net" label="hanmail.net">hanmail.net</option>
+										<option value="naver.com" label="naver.com">naver.com</option>
+										<option value="nate.com" label="nate.com">nate.com</option>
+										<option value="직접입력" label="direct input">직접입력</option>
+									</select>
+								</div>
+							</dd>
+						</dl>
+						<div className="textInput">
+							<input type="text" id="inputLabel1" placeholder="" className="formInput" name="" />
+						</div>
+					</section>
+					<ul className="columnSet selfBottom">
+						<li className="colItem"><button type="button" className="btn primary cancel">취소</button></li>
+						<li className="colItem"><button type="submit" className="btn primary submit" onClick={hideCenterOPen}>확인</button></li>
+					</ul>
 				</div>
 			</div>
 			<div className={props.fixcenter ? "dimmed" : ""}></div>
